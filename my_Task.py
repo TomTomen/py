9891ca3a6c68352b438c6ee4860595ae2136958d
TomTomen <146163285+TomTomen@users.noreply.github.com>
2023-11-05 13:53:03 +0800

    Update my_Task.py
    если хотите через Flask надо редактировать app

--- my_Task.py
+++ my_Task.py
@@ -2,18 +2,59 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
-
+import dash_draggable
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-app = Dash(__name__)
+external_stylesheets = ['https://raw.githubusercontent.com/TomTomen/py/main/styles.css']
 
-app.layout = html.Div([
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+
+app.layout = html.Div(style = {'height': '150vh'}, children  = [
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content'),
-    dcc.Graph(id='top-population', figure = px.bar(df.groupby('country').sum()[['pop']].sort_values('pop', ascending = False)[:15])),
-    dcc.Graph(id='pie-pop', figure = px.pie(values = df.groupby('continent').sum()['pop'].values,names = df.groupby('continent').sum()['pop'].index.tolist())),
+    html.Div([html.Label('Выберите страну:'),dcc.Dropdown(df.country.unique(),value='Canada', multi = True,id='dropdown-selection')], style={'width': '49%', 'display': 'inline-block'}),
+    html.Div([], style={'width': '2%', 'display': 'inline-block'}),
+    html.Div([html.Label('Выберите данные:'),dcc.Dropdown(['lifeExp','pop','gdpPercap'],value='pop',id='dropdown-selection_2')], style={'width': '49%', 'display': 'inline-block'}),
+
+
+    dash_draggable.ResponsiveGridLayout([
+        dcc.Graph(id='graph-content', style={'width': '100%', 'height': '100%'}),
+        dcc.Graph(id='top-population', style={'width': '100%', 'height': '100%'})
+    ],                 ),
+
+    dash_draggable.ResponsiveGridLayout([
+    html.Div([
+        html.Div([
+            html.Label('Выберите цвета:'),
+            dcc.RadioItems(
+                id='radio-selection',
+                options=['country', 'continent'],
+                value='continent',
+                inline=True
+            )
+        ], style={'width': '25%', 'display': 'inline-block'}),
+        html.Div([
+            html.Label('Выберите Y:'),
+            dcc.Dropdown(['lifeExp', 'pop', 'gdpPercap'], value='pop', id='dropdown-selection_Y')
+        ], style={'width': '25%', 'display': 'inline-block'}),
+        html.Div([
+            html.Label('Выберите X:'),
+            dcc.Dropdown(['lifeExp', 'pop', 'gdpPercap'], value='pop', id='dropdown-selection_X')
+        ], style={'width': '25%', 'display': 'inline-block'}),
+        html.Div([
+            html.Label('Выберите Размер:'),
+            dcc.Dropdown(['lifeExp', 'pop', 'gdpPercap'], value='pop', id='dropdown-selection_Size')
+        ], style={'width': '25%', 'display': 'inline-block'}),
+        html.Label(dcc.Graph(id='pie-pop', style={'width': '100%'}))
+    ], style={'height': '100%'}),
+
+    dcc.Graph(id='bubble-chart')
+]),
+
+
+    
     dcc.RangeSlider(id='year-slider',marks={str(year): str(year) for year in df['year']},  # Метки для ползунка
         min=df['year'].min(),
         max=df['year'].max(),
@@ -21,28 +62,87 @@ app.layout = html.Div([
         value=[1978, 2018]  # Значения по умолчанию
     )
 ])
-
+#app.css.append_css({"external_url": "https://raw.githubusercontent.com/TomTomen/py/main/styles.css"})
 @callback(
     Output('graph-content', 'figure'),
     Input('dropdown-selection', 'value'),
+    Input('dropdown-selection_2', 'value'),
     Input('year-slider', 'value')
 
 )
-def update_graph(selected_country, selected_years):
-    dff = df[df.country==selected_country]
+def update_graph(selected_country, measure, selected_years):
+    if not isinstance(selected_country, list):
+         selected_country = [selected_country]
+    dff = df[df['country'].isin(selected_country)]
     dff = dff[(dff['year'] >= selected_years[0]) & (dff['year'] <= selected_years[1])]
-
-    return px.line(dff, x='year', y='pop')
+    return px.line(dff, x='year', y=measure, color='country')
     
-#@callback(
-#    Output('top-population', 'figure'),
-#    [Input('year-slider', 'value')]
-#)
-#def update_figure(selected_years):
-#    filtered_df = df[(df['year'] >= selected_years[0]) & (df['year'] <= selected_years[1])]
-#    fig = px.line(filtered_df, x='year', y='Value')
-#    fig.update_layout(xaxis_tickangle=90)
-#    return fig
+    
+@callback(
+    Output('top-population', 'figure'),
+    Input('dropdown-selection_2', 'value'),
+    Input('year-slider', 'value'),
+    Input('graph-content', 'clickData')
+
+)
+def update_bar(measure, selected_years, point):
+    dff = df[(df['year'] >= selected_years[0]) & (df['year'] <= selected_years[1])]
+    if not point is None:
+        value = point['points'][0]['x']
+        dff =dff[dff.year==value]
+    dff = dff.groupby('country').sum()[[measure]].sort_values(measure, ascending = False)[:15]
+    return px.bar(dff)
+    
+@callback(
+    Output('pie-pop', 'figure'),
+    Input('year-slider', 'value'),
+    Input('graph-content', 'clickData'),
+    Input('radio-selection', 'value'),
+    Input('dropdown-selection_X', 'value'),
+    Input('dropdown-selection_Y', 'value'),
+    Input('dropdown-selection_Size', 'value')
+)
+
+def update_pie(selected_years, point,selected_radio,x,y,size):
+    dff = df[(df['year'] >= selected_years[0]) & (df['year'] <= selected_years[1])]
+    
+    if point is not None:
+        value = point['points'][0]['x']
+        dff = dff[dff['year'] == value]
+
+        
+    if selected_radio == 'continent':
+        fig = px.scatter(dff, x=x, y=y,
+        size=size, color="continent", hover_name="continent",
+        log_x=True,size_max=55)
+        title = 'Континенты'
+        return fig
+    else:
+        fig = px.scatter(dff, x=x, y=y,
+        size=size, color="country", hover_name="country",
+        log_x=True,size_max=55)
+        title = 'Страны'
+        return fig
+#        dff = dff.groupby('country').sum()['pop']
+#        values = dff.values
+#        names = dff.index.tolist()
+#        return px.pie(values=values[:7], names=names[:7])
+
+@callback(
+    Output('bubble-chart', 'figure'),
+    Input('year-slider', 'value'),
+    Input('graph-content', 'clickData')
+
+)
+def update_graph( selected_years,point):
+    dff = df[(df['year'] >= selected_years[0]) & (df['year'] <= selected_years[1])]
+    if not point is None:
+        value = point['points'][0]['x']
+        dff =dff[dff.year==value]    
+    values=dff.groupby('continent').sum()['pop'].values
+    names=dff.groupby('continent').sum()['pop'].index.tolist()
+    return px.pie(dff,values = values, names=names  )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
