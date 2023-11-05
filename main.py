9149dae5ec73f02f3d6da28bfd336d8abc646cc2
TomTomen <146163285+TomTomen@users.noreply.github.com>
2023-11-05 13:51:25 +0800

    Finaly main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+  return """
+    <html>
+        <body>
+            <iframe src="https://ohio.pythonanywhere.com" style="width: 100%; height: 100vh; border: none;"></iframe>
+        </body>
+    </html>
+    """
 if __name__ == "__main__":
     app.run()
